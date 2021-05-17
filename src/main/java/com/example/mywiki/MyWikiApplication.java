0bbbdc067c1a5819a522bb7e8ce96e1bb5ee8e34
A-double-yao <2586307246@qq.com>
2021-05-17 20:22:50 +0800

    2-5 修改启动文案

--- src/main/java/com/example/mywiki/MyWikiApplication.java
+++ src/main/java/com/example/mywiki/MyWikiApplication.java
@@ -1,13 +1,21 @@
 package com.example.mywiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class MyWikiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MyWikiApplication.class, args);
-    }
+    private static final Logger LOG =  LoggerFactory.getLogger(MyWikiApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MyWikiApplication.class);
+        Environment nev = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",nev.getProperty("server.port"));
+    }
 }
