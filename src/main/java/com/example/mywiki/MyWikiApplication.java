284328ba2344f31f1e0dc2481597092951b3ec7e
A-double-yao <2586307246@qq.com>
2021-05-17 17:42:20 +0800

    2-2 init

---
+++ src/main/java/com/example/mywiki/MyWikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mywiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mywiki/MyWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
