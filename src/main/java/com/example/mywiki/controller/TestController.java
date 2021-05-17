2bd66e5f08be093f4590b21cc54b2a60d65e2691
A-double-yao <2586307246@qq.com>
2021-05-17 22:33:41 +0800

    2-7-1 添加了http的测试文件，用于idea中的http页面测试

--- src/main/java/com/example/mywiki/controller/TestController.java
+++ src/main/java/com/example/mywiki/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.example.mywiki.controller;
 
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController //用于返回字符串
 //@Controller    //用于返回页面,前后端分离中一般用不到
@@ -9,5 +13,10 @@ public class TestController {
     @RequestMapping("/test")
     public String test() {
         return "Hello World!";
+    }
+
+    @PostMapping("/test/post")
+    public String PostTest(String name) {
+        return "Hello World!" + name;
     }
 }
