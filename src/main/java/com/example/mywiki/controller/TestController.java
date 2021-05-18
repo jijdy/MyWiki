638cc6a7897ef24ecbcde9edff2b98d05d9a1a64
A-double-yao <2586307246@qq.com>
2021-05-18 14:30:53 +0800

    2-8 增加了自定义配置项的示例：配置文件中的

--- src/main/java/com/example/mywiki/controller/TestController.java
+++ src/main/java/com/example/mywiki/controller/TestController.java
@@ -1,18 +1,21 @@
 package com.example.mywiki.controller;
 
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController //用于返回字符串
 //@Controller    //用于返回页面,前后端分离中一般用不到
 public class TestController {
+
+    //测试xml配置文件在框架中的使用和值传递。
+    @Value("${test.value:defaultValue}") //：之后为默认值
+    private String testValue;
+
     @RequestMapping("/test")
     public String test() {
-        return "Hello World!";
+        return "Hello World!" + testValue;
     }
 
     @PostMapping("/test/post")
