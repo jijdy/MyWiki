05a4235d0b52c82b24a19bd9190a68339a6bc576
A-double-yao <2586307246@qq.com>
2021-05-21 18:19:30 +0800

    4-7 制作the-header与the-footer组件，用于页面统一加载

---
+++ web/src/components/the-header.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys1"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">第一</a-menu-item>
+      <a-menu-item key="2">dier</a-menu-item>
+      <a-menu-item key="3">nav</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-header',
+});
+</script>
