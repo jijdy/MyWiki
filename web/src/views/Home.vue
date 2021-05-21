71870fee836d19ae3fcca0a45eb7d881c1d43da5
A-double-yao <2586307246@qq.com>
2021-05-21 15:47:50 +0800

    4-3 使用vuecli的相关插件添加一个按钮

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <div :style="{ padding: '8px 8px 0 8px', background: 'rgb(190, 200, 200)' }">
+      <a-button ghost>
+        Ghost
+      </a-button>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
