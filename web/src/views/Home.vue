3e9a994f746d799705eb10bdf57b8ebd33d2094f
A-double-yao <2586307246@qq.com>
2021-06-18 17:52:05 +0800

    7-6 【前端】通过将分类的树形结构转换，将其在首页左侧边栏进行分级展示

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -6,44 +6,23 @@
           v-model:selectedKeys="selectedKeys2"
           v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in takeLevel" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
-        </a-sub-menu>
-      </a-menu>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{child.name}}</span>
+          </a-menu-item>
+        </a-sub-menu>>
+      </a-menu>>
     </a-layout-sider>
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
@@ -77,19 +56,9 @@
 import { defineComponent, onMounted,ref} from 'vue'; // @ is an alias to /src
 import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
 import axios from "axios"
+import {Tool} from "@/utils/tool";
+import {message} from "ant-design-vue";
 
-// const listData: Record<string, string>[] = [];
-// for (let i = 0; i < 23; i++) {
-//   listData.push({
-//     href: 'https://www.antdv.com/',
-//     title: `ant design vue part ${i}`,
-//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//     description:
-//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-//     content:
-//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-//   });
-// }
 export default defineComponent({
   components: {
     StarOutlined,
@@ -100,7 +69,35 @@ export default defineComponent({
   setup() {
     const ebooks = ref();
     // const ebooks1 = reactive({bookS: []});
+    // const ebooks1 = reactive({books: []});
+
+    const takeLevel =  ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          takeLevel.value = [];
+          takeLevel.value = Tool.arrayToTree(categorys, 0);
+          console.log("树形结构：", takeLevel.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click")
+    };
+
     onMounted(() => {
+      handleQueryCategory();
       axios.get("/ebook/list", {
             params: {
               page: 1,
@@ -131,8 +128,10 @@ export default defineComponent({
       // listData,
       pagination,
       actions,
-      ebooks,                           //没有用到的测试数据
-      // books: toRef(ebooks1,"bookS"),//同上
+      ebooks,
+      handleClick,
+      takeLevel,
+
     };
   }
 });
