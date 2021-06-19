dddba1e1492dcbf357194120188914ec20fb3d01
A-double-yao <2586307246@qq.com>
2021-06-19 13:29:41 +0800

    【后端】增加文档表sql，并增加该表的持久层代码

---
+++ src/main/java/com/example/mywiki/mapper/DocMapper.java
@@ -0,0 +1,31 @@
+package com.example.mywiki.mapper;
+
+import com.example.mywiki.domain.Doc;
+import com.example.mywiki.domain.DocExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface DocMapper {
+    long countByExample(DocExample example);
+
+    int deleteByExample(DocExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Doc record);
+
+    int insertSelective(Doc record);
+
+    List<Doc> selectByExample(DocExample example);
+
+    Doc selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Doc record, @Param("example") DocExample example);
+
+    int updateByExample(@Param("record") Doc record, @Param("example") DocExample example);
+
+    int updateByPrimaryKeySelective(Doc record);
+
+    int updateByPrimaryKey(Doc record);
+}
