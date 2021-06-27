7ad1ef0883c59fe7c90ecf840f84f88efd9d05a8
A-double-yao <2586307246@qq.com>
2021-06-27 16:04:47 +0800

    【前端】前端增加拦截，未登录时不能够访问到管理页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,6 +6,8 @@ import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
 import Doc from '../views/doc.vue'
 import User from "../views/admin/admin-user.vue"
+import store from "@/store";
+import {Tool} from "@/utils/tool";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,22 +18,34 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/user',
     name: 'user',
-    component: User
+    component: User,
+      meta: {
+          loginRequire: true
+      }
   },
   {
     path: '/about',
     name: 'About',
-    component: About
+    component: About,
+      meta: {
+          loginRequire: true
+      }
   },
   {
   path: '/admin',
       name: 'AdminEbook',
-      component: AdminEbook
+      component: AdminEbook,
+      meta: {
+          loginRequire: true
+      }
   },
   {
   path: '/category',
       name: 'AdminCategory',
-      component: AdminCategory
+      component: AdminCategory,
+      meta: {
+          loginRequire: true
+      }
   },
   {
   path: '/doc',
@@ -50,4 +64,22 @@ const router = createRouter({
   routes
 })
 
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+    // 要不要对meta.loginRequire属性做监控拦截
+    if (to.matched.some(function (item) {
+        console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+        return item.meta.loginRequire
+    })) {
+        const loginUser = store.state.user;
+        if (Tool.isEmpty(loginUser)) {
+            console.log("用户未登录！");
+            next('/');
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
 export default router
