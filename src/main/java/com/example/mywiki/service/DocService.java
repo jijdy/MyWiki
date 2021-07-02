8e1c45ef38e02a32df461ffa3f8e2712e2e06e83
A-double-yao <2586307246@qq.com>
2021-07-02 16:04:51 +0800

    【后端】天剑事务注解

--- src/main/java/com/example/mywiki/service/DocService.java
+++ src/main/java/com/example/mywiki/service/DocService.java
@@ -19,6 +19,7 @@ import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -86,6 +87,7 @@ public class DocService {
     }
 
     //保存文档信息
+    @Transactional
     public void save(DocSaveReq req) {
         Log.info(req.toString());
         Doc doc = CopyUtil.copy(req, Doc.class);
