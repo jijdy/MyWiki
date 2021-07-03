fe23e03377beae35e57d91ed9183a8b3cf6481cb
A-double-yao <2586307246@qq.com>
2021-07-03 19:06:11 +0800

    【前端】关于作者栏更改

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>关于作者</h1>
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <h1>
+        一个还在求职路上的初学者......
+      </h1>
+    </a-layout-content>
+  </a-layout>
 </template>
